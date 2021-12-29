df420c83b0090bd85ee35b5fe89c896154c50bd8
Margarita Novokh <r28611@gmail.com>
2021-12-29 18:56:05 +0100

    added Model

---
+++ ToDo Manager/Model/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  ToDo Manager
+//
+//  Created by Margarita Novokhatskaia on 29/12/2021.
+//
+
+import Foundation
+
+enum TaskPriority {
+    case normal
+    case important
+}
+
+enum TaskStatus {
+    case planned
+    case completed
+}
+
+protocol TaskProtocol {
+    var title: String { get set }
+    var type: TaskPriority { get set }
+    var status: TaskStatus { get set }
+}
+
+struct Task: TaskProtocol {
+    var title: String
+    var type: TaskPriority
+    var status: TaskStatus
+}
---
+++ ToDo Manager/Model/TasksStorage.swift
@@ -0,0 +1,30 @@
+//
+//  TasksStorage.swift
+//  ToDo Manager
+//
+//  Created by Margarita Novokhatskaia on 29/12/2021.
+//
+
+import Foundation
+
+protocol TasksStorageProtocol {
+    func loadTasks() -> [TaskProtocol]
+    func saveTasks(_ tasks: [TaskProtocol])
+}
+
+class TasksStorage: TasksStorageProtocol {
+    
+    func loadTasks() -> [TaskProtocol] {
+        let testTasks: [TaskProtocol] = [
+            Task(title: "Купить хлеб", type: .normal, status: .planned),
+            Task(title: "Помыть кота", type: .important, status: .planned),
+            Task(title: "Отдать долг Арнольду", type: .important, status: .completed),
+            Task(title: "Купить новый пылесос", type: .normal, status: .completed),
+            Task(title: "Подарить цветы супруге", type: .important, status: .planned),
+            Task(title: "Позвонить родителям", type: .important, status: .planned)
+        ]
+        return testTasks
+    }
+    
+    func saveTasks(_ tasks: [TaskProtocol]) {}
+}
