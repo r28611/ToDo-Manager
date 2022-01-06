71197570f7e834d2481bb474a8aabf0c630d2eaa
Margarita Novokh <r28611@gmail.com>
2022-01-06 20:14:18 +0100

    setup cells for editing Task title, type

--- ToDo Manager/Controller/TaskEditController.swift
+++ ToDo Manager/Controller/TaskEditController.swift
@@ -14,6 +14,32 @@ class TaskEditController: UITableViewController {
     var taskStatus: TaskStatus = .planned
     var doAfterEdit: ((String, TaskPriority, TaskStatus) -> Void)?
     
+    private let taskTitle: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Введите задачу"
+        textField.borderStyle = .none
+        textField.font = .systemFont(ofSize: 17)
+        return textField
+    }()
+    
+    private let typeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Тип задачи"
+        return label
+    }()
+    
+    private let taskTypeLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private var taskTitles: [TaskPriority:String] = [
+        .important: "Важная",
+        .normal: "Текущая"
+    ]
+    
     // MARK: - Initialization
     
     init() {
@@ -33,6 +59,8 @@ class TaskEditController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = .systemGroupedBackground
+        taskTitle.text = taskText
+        taskTypeLabel.text = taskTitles[taskType]
     }
 
     // MARK: - Table view data source
@@ -47,8 +75,23 @@ class TaskEditController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-
-        // Configure the cell...
+        switch indexPath.row {
+        case 0:
+            taskTitle.frame = cell.bounds.insetBy(dx: 16, dy: 0)
+            cell.addSubview(taskTitle)
+        case 1:
+            cell.accessoryType = .disclosureIndicator
+            typeLabel.frame = cell.bounds.insetBy(dx: 16, dy: 0)
+            taskTypeLabel.frame = cell.bounds.insetBy(dx: 8, dy: 0)
+            cell.addSubview(typeLabel)
+            cell.addSubview(taskTypeLabel)
+        case 2:
+            let textField = UITextField()
+            cell.contentView.addSubview(textField)
+        default:
+            return cell
+        }
+        
 
         return cell
     }
